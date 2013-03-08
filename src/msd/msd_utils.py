8327687ad4f8ef71807522d323476678b439f858
Jussi Kukkonen <jussi.kukkonen@intel.com>
2013-03-08 19:59:01 +0200

    [Utils] make remote image loading asynchronous
    Remote images were previously loaded synchronously in
    image_from_file(). Change that into a PixbufAsyncLoader that takes a
    url and a callback. Callback will be called when pixbuf is ready.
    
    Fixes #12.
    
    Signed-off-by: Jussi Kukkonen <jussi.kukkonen@intel.com>

--- src/msd/msd_utils.py
+++ src/msd/msd_utils.py
@@ -16,26 +16,41 @@
 # 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # Mark Ryan <mark.d.ryan@intel.com>
+# Jussi Kukkonen <jussi.kukkonen@intel.com>
 #
 
-import tempfile
 import pygtk
 pygtk.require('2.0')
 import gtk
-import urllib2
-import os
+import gio
 
-def image_from_file(url):
-    tmpfile = tempfile.NamedTemporaryFile(delete=False)
-    tmpFileName = tmpfile.name
-    image = None
-    try:
-        with tmpfile:
-            message = urllib2.urlopen(url, None, 15)
-            tmpfile.write(message.read())
-        image = gtk.Image()
-        image.set_from_file(tmpfile.name)
-    finally:
-        os.unlink(tmpFileName)
+# loads a pixbuf from given url, calls callback() when pixbuf is ready
+class PixbufAsyncLoader():
+    def __on_stream_read(self, stream, result):
+        try:
+            buf = stream.read_finish(result)
+            if buf:
+                self.__loader.write (buf)
+                stream.read_async(10000, self.__on_stream_read)
+            else:
+                self.__loader.close ()
+                self.__callback (self.__loader.get_pixbuf(), self.__userdata)
+        except:
+            print "Failed to load image: %s" % err.message
+            pass
 
-    return image
+    def __on_file_read(self, image_file, result):
+        try:
+            stream = image_file.read_finish(result)
+            stream.read_async(10000, self.__on_stream_read)
+            self.__loader = gtk.gdk.PixbufLoader()
+        except Exception as err:
+            print "Failed to load image '%s': %s" % (image_file.get_uri(),
+                                                     err.message)
+            pass
+
+    def __init__(self, url, callback, userdata=None):
+        self.__callback = callback
+        self.__userdata = userdata
+        img_file = gio.File(uri=url)
+        img_file.read_async(self.__on_file_read)
