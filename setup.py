652726d6eecd76b6819b9c7043be526672969c04
Regis Merlino <regis.merlino@intel.com>
2013-08-22 16:55:42 +0200

    [Configuration] Post-release version bump
    Signed-off-by: Regis Merlino <regis.merlino@intel.com>

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ class MSDInstallData(install_data):
         install_data.finalize_options(self)
 
 setup(name = "msd",
-      version = "0.2.0",
+      version = "0.2.1",
       description = "Test DMP to demonstrate features of dleyna-server",
       author = "Mark Ryan",
       author_email = "mark.d.ryan@intel.com",
