dcb5427ba7ac6bacbac0b95459da9504c1ec8865
Larry Sachs <larry.j.sachs@intel.com>
2015-01-07 09:26:33 -0800

    script to launch app without proxy setting

---
+++ src/launch-without-proxy-settings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# launches an app after first clearing proxy settings
+# for example, launch-without-proxy-settings gupnp-av-cp
+
+
+import os
+import sys
+from subprocess import call
+
+
+try:
+  #print os.environ;
+  del os.environ["http_proxy"];
+  del os.environ["https_proxy"];
+  del os.environ["ftp_proxy"];
+  del os.environ["socks_proxy"];
+  del os.environ["no_proxy"];
+
+  print '\n';
+  print '\n';
+
+  #print os.environ;
+except Exception, e:
+  print e;
+  pass
+
+if len(sys.argv) >= 1:
+  print "launching " + sys.argv[1];
+  call([sys.argv[1]]);
+
